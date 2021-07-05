97619d1876e2e39bf587436a6cf477b380ced30a
navaneeth <navaneethbhasuram@gmail.com>
2021-07-05 20:09:57 +0530

    Empty Controller,Model,Collection Route,View,Exceptions Added

--- public/index.php
+++ public/index.php
@@ -6,6 +6,10 @@ use App\Application;
 require_once '../vendor/autoload.php';
 session_start();
 
+const STORAGE_PATH = __DIR__.'/../storage';
+const VIEW_PATH = __DIR__.'/../views';
+const UPLOAD_PATH = STORAGE_PATH.'/uploads';
+
 /*set_exception_handler(
     function (\Throwable $exception) {
         var_dump('global exception-:<br>'.$exception->getMessage());
@@ -19,7 +23,10 @@ $app->router->register(
     function () {
         echo App\View::make('welcome');
     }
-)->view('intro','welcome');
+)->view('intro','welcome')
+    ->register('get', '/index', [FormController::class, 'index'])
+    ->get('/create', [FormController::class, 'create'])
+    ->post('/create', [FormController::class, 'store']);
+
 
 $app->run();
-
